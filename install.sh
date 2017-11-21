63e38cdc2bc5c55e23f7b2817ff76e68f0ef191a
Mikey <mikey@Mikeys-MacBook-Pro.local>
2017-11-21 16:15:04 -0500

    added brew pkg

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "INSTALLING BREW"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python3 neovim git screenfetch
+brew install python3 neovim git screenfetch ranger
 
 echo "SYMLINKS"
 rm -f ~/.bashrc
