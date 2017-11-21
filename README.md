9d0f6888d082a45b1b042b9a4f0d3e23c0f92fbb
Mikey <mikey@Mikeys-MacBook-Pro.local>
2017-11-21 14:41:44 -0500

    initial version
    edit readme
    
    finished up
    
    accounted for repository name change
    
    v1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Macbook Dotfiles & Install
+## IMPORTANT: Rename this folder to dotfiles and move it to user home (which is probably the folder that is your name, one up from the Desktop folder, it's ~/)
+
+~~~
+cd ~/
+git clone https://github.com/otmichael/dotfiles_osx.git
+mv ~/dotfiles_osx ~/dotfiles
+chmod +x install.sh
+chmod +x manual_commands.sh
+./install.sh
+~~~
--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 !/bin/bash
 
+echo "INSTALLING BREW"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python3 neovim git screenfetch
+
+echo "SYMLINKS"
 rm -f ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 
@@ -15,8 +20,8 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sudo easy_install pip
-/usr/bin/yes | sudo pip uninstall neovim
-sudo pip install neovim
+/usr/bin/yes | sudo pip3 uninstall neovim
+sudo pip3 install neovim
 
 echo " INSTALLING NODE VERSION MANAGER"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
@@ -27,7 +32,11 @@ nvm install node
 echo "INSTALLING NPM PACKAGES"
 npm install -g yarn pm2 webpack jest
 
+echo "SHOW HIDDEN FILES"
+defaults write com.apple.finder AppleShowAllFiles YES
+
 echo "SOURCING"
 source ~/.bashrc
 
 echo "DONE."
+./manual_commands.sh
